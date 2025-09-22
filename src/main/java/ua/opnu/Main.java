0283cc7c3317270879c38df42e9683245e2525ec
Bogdan8138 <9127167@stud.op.edu.ua>
2025-09-22 19:04:48 +0300

    Update Main.java

--- src/main/java/ua/opnu/Main.java
+++ src/main/java/ua/opnu/Main.java
@@ -2,5 +2,14 @@ package ua.opnu;
 
 public class Main {
     public static void main(String[] args) {
+        Student s = new Student("Паламарчук Богдан Олександрович", 2);
+
+        s.addCourse("Організація баз даних та знань");
+        s.addCourse("Об’єктно-орієнтоване програмування");
+        s.addCourse("Теорія ймовірностей");
+
+        System.out.println(s.getName() + ": кількість вивчаємих дисциплін - " + s.getCourseCount());
+        System.out.println(s.getName() + ": рік навчання - " + s.getYear());
+        System.out.println(s.getName() + ": заплатив за навчання - " + s.getTuition());
     }
 }
